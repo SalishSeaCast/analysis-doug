dfcb7967953a528f2d928520f5d8d75e153e0f69
Doug Latornell <dlatornell@eoas.ubc.ca>
2018-10-14 12:27:22 -0700

    Update README.

--- notebooks/make_readme.py
+++ notebooks/make_readme.py
@@ -23,7 +23,7 @@ import re
 
 
 nbviewer = 'http://nbviewer.jupyter.org/urls'
-repo = 'bitbucket.org/salishsea/analysis-doug/raw/tip'
+repo = 'bitbucket.org/salishsea/analysis-doug/raw/default'
 repo_dir = 'notebooks'
 url = os.path.join(nbviewer, repo, repo_dir)
 title_pattern = re.compile('#{1,6} ?')
